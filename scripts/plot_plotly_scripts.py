ef8220f87916aa7c28fc669bc8f23a55af4ea746
SemyonSinchenko <semyonsinchenko@yahoo.com>
2020-11-28 06:55:43 +0300

    Fix imports 3
    On branch master
    Changes to be committed:
    modified:   scripts/plot_plotly_scripts.py

--- scripts/plot_plotly_scripts.py
+++ scripts/plot_plotly_scripts.py
@@ -1,10 +1,12 @@
+from typing import Dict
+
 import fbprophet as fbp
 import pandas as pd
 from fbprophet import plot
 from plotly.io import to_html
 
 
-def create_plots(model: fbp.Prophet, fcst: pd.DataFrame) -> Dicr[str, str]:
+def create_plots(model: fbp.Prophet, fcst: pd.DataFrame) -> Dict[str, str]:
     forecast = plot.plot_plotly(model, fcst, xlabel="Date", ylabel="Forecast")
     trend = plot.plot_forecast_component_plotly(model, fcst, name="trend")
     daily = plot.plot_seasonality_plotly(model, fcst, name="daily")
