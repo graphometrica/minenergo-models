0b942aaab3f250cf33ecd42cfa36ddf8ad479568
SemyonSinchenko <semyonsinchenko@yahoo.com>
2020-11-28 06:57:51 +0300

    Fix 1
    On branch master
    Changes to be committed:
    modified:   scripts/plot_plotly_scripts.py

--- scripts/plot_plotly_scripts.py
+++ scripts/plot_plotly_scripts.py
@@ -9,8 +9,8 @@ from plotly.io import to_html
 def create_plots(model: fbp.Prophet, fcst: pd.DataFrame) -> Dict[str, str]:
     forecast = plot.plot_plotly(model, fcst, xlabel="Date", ylabel="Forecast")
     trend = plot.plot_forecast_component_plotly(model, fcst, name="trend")
-    daily = plot.plot_seasonality_plotly(model, fcst, name="daily")
-    weekly = plot.plot_seasonality_plotly(model, fcst, name="weekly")
+    daily = plot.plot_forecast_component_plotly(model, fcst, name="daily")
+    weekly = plot.plot_forecast_component_plotly(model, fcst, name="weekly")
 
     return dict(
         forecast=to_html(forecast),
