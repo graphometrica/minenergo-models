6e32610573c4009288c9a881f4ee233e65358abb
SemyonSinchenko <semyonsinchenko@yahoo.com>
2020-11-28 05:36:20 +0300

    Fix 5
    On branch master
    Changes to be committed:
    modified:   main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_data(region: int, con, global_df: pd.DataFrame) -> pd.DataFrame:
     now = datetime.now()
     max_dt = global_df["ds"].max().to_pydatetime()
     if (now - max_dt).seconds / 3600 > 1:
-        query_cond = now.strftime(fmt="%Y-%m-%d %H:00:00")
+        query_cond = now.strftime("%Y-%m-%d %H:00:00")
         new_data = pd.read_sql(
             f"select * from minenergo.joined where ds='{query_cond}'", con=con
         )
